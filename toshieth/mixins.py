05cd149ce49851180b76f71147461fc47998dd22
Sunjin Lee <styner32@gmail.com>
2018-09-14 16:54:40 +0900

    Quick hack to use infura for `eth_getBalance` call

--- toshieth/mixins.py
+++ toshieth/mixins.py
@@ -1,4 +1,35 @@
+import tornado.httpclient
+import regex
+import os
+
+from toshi.log import log
 from toshi.utils import parse_int
+
+JSON_RPC_VERSION = "2.0"
+HEX_RE = regex.compile("(0x)?([0-9a-fA-F]+)")
+
+def validate_hex(value):
+    if isinstance(value, int):
+        if value < 0:
+            raise ValueError("Negative values are unsupported")
+        value = hex(value)[2:]
+    if isinstance(value, bytes):
+        value = binascii.b2a_hex(value).decode('ascii')
+    else:
+        m = HEX_RE.match(value)
+        if m:
+            value = m.group(2)
+        else:
+            raise ValueError("Unable to convert value to valid hex string")
+
+    return '0x' + value
+
+def validate_block_param(param):
+    if param not in ("earliest", "latest", "pending"):
+        return validate_hex(param)
+    return param
+
+httpCli = tornado.httpclient.AsyncHTTPClient(max_clients=100)
 
 class BalanceMixin:
 
@@ -42,8 +73,33 @@ class BalanceMixin:
                                for p in pending_received
                                if include_queued or p['status'] == 'unconfirmed')
 
-        confirmed_balance = await self.eth.eth_getBalance(eth_address, block=block or "latest")
+        confirmed_balance = 0
+        if os.getenv('USE_INFURA', 'false') == 'true':
+            confirmed_balance = await self.getEthBalance(eth_address, block=block or "latest")
+        else:
+            confirmed_balance = await self.eth.eth_getBalance(eth_address, block=block or "latest")
 
         balance = (confirmed_balance + pending_received) - pending_sent
 
         return confirmed_balance, balance, pending_sent, pending_received
+
+    async def getEthBalance(self, address, block="latest"):
+        data = {
+            "jsonrpc": JSON_RPC_VERSION,
+            "id": 1,
+            "method": 'eth_getBalance',
+            "params": [address, validate_block_param(block)]
+        }
+
+        try:
+            resp = await httpCli.fetch(
+                'https://mainnet.infura.io',
+                method="POST",
+                headers={'Content-Type': "application/json"},
+                body=tornado.escape.json_encode(data)
+            )
+        except Exception as e:
+            log.warning("Error in JsonRPCClient._fetch ({}, {}) \"{}\"".format(data['method'], data['params'], str(e)))
+
+        rval = tornado.escape.json_decode(resp.body)
+        return parse_int(rval['result'])
